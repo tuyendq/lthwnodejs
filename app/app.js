a537a59f9d28ae3f3d0b054051e7d20e865c971f
Tuyen Dang <tuyendq@gmail.com>
2018-07-29 14:40:04 +0700

    (1): add favicon.ico; (2): install view engine ejs; (3) add view: index.ejs; (4): Add partial templates: head.ejs, header.ejs, footer.ejs, bootstrap.ejs; (5): add app/public/styles/style.css

--- app/app.js
+++ app/app.js
@@ -3,6 +3,9 @@ var express = require('express');
 var app = express();
 
 app.set('port', process.env.PORT || 1337);
+// Set view engine to ejs; views is in app/views folder
+app.set('view engine', 'ejs');
+app.set('views', 'app/views');
 
 app.locals.siteTitle = "LTHW NodeJS";
 
