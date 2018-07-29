5fc80bc928295f0d8dd2d07d0504b409b50a8f19
Tuyen Dang <tuyendq@gmail.com>
2018-07-29 22:14:15 +0700

    rename app.js to index.js, and modify package.json

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
-var http = require('http');
+var express = require('express');
 
-var server = http.createServer(function(request,response){
-    response.writeHead(200, {
-        "Content-Type": "text/plain"
-    });
-    response.end("Hello nodejs on Azure FREE Tier!")
+var app = express();
+
+app.set('port', process.env.PORT || 1337);
+// Set view engine to ejs; views is in app/views folder
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+app.locals.siteTitle = "LTHW NodeJS";
+
+app.use(express.static('./public'));
+app.use(require('./routes/index'));
+
+
+var server = app.listen(app.get('port'), function(){
+    console.log('Listening on port ' + app.get('port'));
 });
 
-var port = process.env.PORT || 1337;
 
-server.listen(port);
 
-console.log("Server running at http://localhost:%d", port);
