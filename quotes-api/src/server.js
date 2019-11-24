8c6f39fe34cefb94bd64e2dc519238cc4e48e5ce
Tuyen Dang <tuyendq@gmail.com>
2019-11-24 11:59:02 +0700

    Add POST route /quotes

--- quotes-api/src/server.js
+++ quotes-api/src/server.js
@@ -2,12 +2,15 @@
 
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 var port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 var quotes = [
 {id: 1,quote: "The best is yet to come", author: "Unknown", year: 2000},
 {id: 2,quote: "Everything is practice.", author: "Pele", year: 1950},
-{id: 1,quote: "You're what you practice most.", author: "Unknown", year: 1910}
+{id: 3,quote: "You're what you practice most.", author: "Unknown", year: 1910}
 ];
 
 app.listen(port, function(){
@@ -16,21 +19,27 @@ app.listen(port, function(){
 
 // Route /
 app.get('/', function(request, response){
-	response.send('Get request received at "/"');
+    response.send('Get request received at "/"');
 });
 
 // Route /quotes
 app.get('/quotes', function(req, res){
-	if (req.query.year){
-		res.send('Return a list of quotes from yaar: ' + req.query.year);
-	} else {
-	console.log('Get list of all quotes as json');
-	res.json(quotes);
-	}
+    if (req.query.year){
+        res.send('Return a list of quotes from yaar: ' + req.query.year);
+    } else {
+    console.log('Get list of all quotes as json');
+    res.json(quotes);
+    }
 });
 
 // Route /quotes:id
 app.get('/quotes/:id', function(req, res){
 	console.log('Return quote with id: ' + req.params.id);
 	res.send('Return quote with id: ' + req.params.id);
+});
+
+// Route POST /quotes
+app.post('/quotes', function(req, res){
+    console.log('Insert a new quote: ' + req.body.quote);
+    res.json(req.body);
 });
